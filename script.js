1e92cecf18ff7e123b198876ebe0283673b6d04f
StainleySW <szwtron@gmail.com>
2020-10-08 16:58:31 +0700

    js documented

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var selectedRow = null
 
+//generate tabel dari JSON
 function rendertabelmahasiswa(dataawal){
     let index = 0;
     let tbody = document.getElementById("studentlist").querySelector('tbody');
@@ -16,6 +17,181 @@ function rendertabelmahasiswa(dataawal){
     }
 }
 
+//memanggil file JSON
+function onDocFinish(){
+    loadfirstdata();
+}
+
+function loadfirstdata(){
+    let request = new XMLHttpRequest();
+    request.open('GET','first.json', true);
+
+    request.onload = function(){
+        if(request.status >= 200 && request.status < 400){
+            let dataawal = JSON.parse(request.responseText);
+            console.log(dataawal);
+            rendertabelmahasiswa(dataawal);
+        }
+        else{
+            alert('Page Not Found');
+        }
+    }
+    request.onerror = function(){
+        alert('Request Failed! Check your internet connection');
+    }
+    request.send();
+}
+
+//menentukan untuk masuk ke update data atau insert new data
+function onModalSubmit(){
+    if(validatemodalbox()){
+        var formData = readEditData();
+    if(selectedRow==null)
+        insertNewData(formData);
+        else
+        updateData(formData);
+    }
+}
+
+function onFormSubmit(){
+    if (validate()){
+        var formData = readFormData();
+    if(selectedRow==null)
+        insertNewData(formData);
+        else
+        updateEditData(formData);
+
+    clearForm();
+    }
+}
+
+//read untuk submit form
+function readFormData(){
+    var formData = {};
+    formData["namamahasiswa"] = document.getElementById("namamahasiswa").value;
+    formData["NomorInduk"] = document.getElementById("NomorInduk").value;
+    formData["Jurusan"] = document.getElementById("Jurusan").value;
+    formData["Angkatan"] = document.getElementById("Angkatan").value;
+    return formData
+}
+
+//read data untuk modal box
+function readEditData(){
+    var formData = {};
+    formData["namahasiswa"] = document.getElementById("namahasiswa").value;
+    formData["NoInduk"] = document.getElementById("NoInduk").value;
+    formData["Jurus"] = document.getElementById("Jurus").value;
+    formData["Angkat"] = document.getElementById("Angkat").value;
+    return formData
+}
+
+//memasukkan data baru ke tabel
+function insertNewData(data){
+    var table = document.getElementById("studentlist").getElementsByTagName('tbody')[0];
+    var newrow = table.insertRow(table.length);
+    cell1 = newrow.insertCell(0);
+    cell1.innerHTML = data.namamahasiswa;
+    cell2 = newrow.insertCell(1);
+    cell2.innerHTML = data.NomorInduk;
+    cell3 = newrow.insertCell(2);
+    cell3.innerHTML = data.Jurusan;
+    cell4 = newrow.insertCell(3);
+    cell4.innerHTML = data.Angkatan;
+    cell4 = newrow.insertCell(4);
+    cell4.innerHTML = `<button onClick="ModalEdit(this)" class="editbutton">Edit</button><br>
+                       <button onClick="Delete(this);alertDelete()" class="deletebutton">Delete</button>`;
+}
+
+//membersihkan form
+function clearForm(){
+    document.getElementById("namamahasiswa").value = "";
+    document.getElementById("NomorInduk").value = "";
+    document.getElementById("Jurusan").value = "";
+    document.getElementById("Angkatan").value = "";
+}
+
+//memasukkan data ke modal box
+function ModalEdit(td){
+    var modalBg = document.querySelector('.modalbox');
+    modalBg.classList.add('boxactive');
+    document.getElementById("modalbox").style.display="inline-flex";
+    selectedRow = td.parentElement.parentElement;
+    console.log(selectedRow);
+    document.getElementById("namahasiswa").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("NoInduk").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("Jurus").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("Angkat").value = selectedRow.cells[3].innerHTML;
+}
+
+function updateData(formData){
+    selectedRow.cells[0].innerHTML = formData.namahasiswa;
+    selectedRow.cells[1].innerHTML = formData.NoInduk;
+    selectedRow.cells[2].innerHTML = formData.Jurus;
+    selectedRow.cells[3].innerHTML = formData.Angkat;
+}
+
+function updateEditData(formData){
+    selectedRow.cells[0].innerHTML = formData.namamahasiswa;
+    selectedRow.cells[1].innerHTML = formData.NomorInduk;
+    selectedRow.cells[2].innerHTML = formData.Jurusan;
+    selectedRow.cells[3].innerHTML = formData.Angkatan;
+}
+
+//menghapus data
+function Delete(td){
+    if(confirm('Hapus data ini ?')){
+        row = td.parentElement.parentElement;
+        document.getElementById("studentlist").deleteRow(row.rowIndex);
+        clearForm();
+    } 
+}
+
+//validasi form submit
+function validate() {
+    isValid = true;
+    if (document.getElementById("namamahasiswa").value == ""||document.getElementById("NomorInduk").value  == ""
+        ||document.getElementById("Jurusan").value ==""||document.getElementById("Angkatan").value =="") {
+        isValid = false;
+        alert("Semua Data Wajib Diisi");
+        insfailalert();
+    } else {
+        isValid = true;
+        insalert();
+    }
+    return isValid;
+}
+
+
+//validasi form modal box
+function validatemodalbox() {
+    isValid = true;
+    if (document.getElementById("namahasiswa").value == ""|| document.getElementById("NoInduk").value == ""
+        ||document.getElementById("Jurus").value ==""||document.getElementById("Angkat").value=="" ) {
+        isValid = false;
+        alert("Semua Data Wajib Diisi");
+        editfailalert();
+    } else {
+        isValid = true;
+        editalert();
+    }
+    return isValid;
+}
+
+//show modalbox
+function modalclose(){
+    var modalBg = document.querySelector('.modalbox');
+    modalBg.classList.remove('boxactive');
+}
+
+//close modalbox
+var closemodalbox = document.querySelector('.closemodal');
+
+closemodalbox.addEventListener('click',function(){
+    var modalBg = document.querySelector('.modalbox');
+    modalBg.classList.remove('boxactive');
+})
+
+//show alert"
 function alertDelete(){
     var delalert = document.querySelector('.alertdelete');
     delalert.classList.add('boxactive');
@@ -41,176 +217,7 @@ function insfailalert(){
     infalert.classList.add('boxactive');
 }
 
-function loadfirstdata(){
-    let request = new XMLHttpRequest();
-    request.open('GET','first.json', true);
-
-    request.onload = function(){
-        if(request.status >= 200 && request.status < 400){
-            let dataawal = JSON.parse(request.responseText);
-            console.log(dataawal);
-            rendertabelmahasiswa(dataawal);
-        }
-        else{
-            alert('Page Not Found');
-        }
-    }
-    request.onerror = function(){
-        alert('Request Failed! Check your internet connection');
-    }
-    request.send();
-}
-
-function onDocFinish(){
-    loadfirstdata();
-}
-
-function onModalSubmit(){
-    if(validatemodalbox()){
-        var formData = readEditData();
-    if(selectedRow==null)
-        insertNewData(formData);
-        else
-        updateData(formData);
-    }
-}
-
-function onFormSubmit(){
-    if (validate()){
-        var formData = readFormData();
-    if(selectedRow==null)
-        insertNewData(formData);
-        else
-        updateEditData(formData);
-
-    clearForm();
-    }
-}
-
-function readFormData(){
-    var formData = {};
-    formData["namamahasiswa"] = document.getElementById("namamahasiswa").value;
-    formData["NomorInduk"] = document.getElementById("NomorInduk").value;
-    formData["Jurusan"] = document.getElementById("Jurusan").value;
-    formData["Angkatan"] = document.getElementById("Angkatan").value;
-    return formData
-}
-
-function readEditData(){
-    console.log('test')
-    var formData = {};
-    formData["namahasiswa"] = document.getElementById("namahasiswa").value;
-    formData["NoInduk"] = document.getElementById("NoInduk").value;
-    formData["Jurus"] = document.getElementById("Jurus").value;
-    formData["Angkat"] = document.getElementById("Angkat").value;
-    return formData
-}
-
-function insertNewData(data){
-    var table = document.getElementById("studentlist").getElementsByTagName('tbody')[0];
-    var newrow = table.insertRow(table.length);
-    cell1 = newrow.insertCell(0);
-    cell1.innerHTML = data.namamahasiswa;
-    cell2 = newrow.insertCell(1);
-    cell2.innerHTML = data.NomorInduk;
-    cell3 = newrow.insertCell(2);
-    cell3.innerHTML = data.Jurusan;
-    cell4 = newrow.insertCell(3);
-    cell4.innerHTML = data.Angkatan;
-    cell4 = newrow.insertCell(4);
-    cell4.innerHTML = `<button onClick="ModalEdit(this)" class="editbutton">Edit</button><br>
-                       <button onClick="Delete(this);alertDelete()" class="deletebutton">Delete</button>`;
-}
-
-function clearForm(){
-    document.getElementById("namamahasiswa").value = "";
-    document.getElementById("NomorInduk").value = "";
-    document.getElementById("Jurusan").value = "";
-    document.getElementById("Angkatan").value = "";
-}
-
-function ModalEdit(td){
-    var modalBg = document.querySelector('.modalbox');
-    modalBg.classList.add('boxactive');
-    document.getElementById("modalbox").style.display="inline-flex";
-    selectedRow = td.parentElement.parentElement;
-    console.log(selectedRow);
-    document.getElementById("namahasiswa").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("NoInduk").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("Jurus").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("Angkat").value = selectedRow.cells[3].innerHTML;
-}
-
-function Edit(td){
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("namamahasiswa").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("NomorInduk").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("Jurusan").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("Angkatan").value = selectedRow.cells[3].innerHTML;
-}
-
-function updateData(formData){
-    selectedRow.cells[0].innerHTML = formData.namahasiswa;
-    selectedRow.cells[1].innerHTML = formData.NoInduk;
-    selectedRow.cells[2].innerHTML = formData.Jurus;
-    selectedRow.cells[3].innerHTML = formData.Angkat;
-}
-
-function updateEditData(formData){
-    selectedRow.cells[0].innerHTML = formData.namamahasiswa;
-    selectedRow.cells[1].innerHTML = formData.NomorInduk;
-    selectedRow.cells[2].innerHTML = formData.Jurusan;
-    selectedRow.cells[3].innerHTML = formData.Angkatan;
-}
-
-function Delete(td){
-    if(confirm('Hapus data ini ?')){
-        row = td.parentElement.parentElement;
-        document.getElementById("studentlist").deleteRow(row.rowIndex);
-        clearForm();
-    } 
-}
-
-function validate() {
-    isValid = true;
-    if (document.getElementById("namamahasiswa").value == ""||document.getElementById("NomorInduk").value  == ""
-        ||document.getElementById("Jurusan").value ==""||document.getElementById("Angkatan").value =="") {
-        isValid = false;
-        alert("Semua Data Wajib Diisi");
-        insfailalert();
-    } else {
-        isValid = true;
-        insalert();
-    }
-    return isValid;
-}
-
-function validatemodalbox() {
-    isValid = true;
-    if (document.getElementById("namahasiswa").value == ""|| document.getElementById("NoInduk").value == ""
-        ||document.getElementById("Jurus").value ==""||document.getElementById("Angkat").value=="" ) {
-        isValid = false;
-        alert("Semua Data Wajib Diisi");
-        editfailalert();
-    } else {
-        isValid = true;
-        editalert();
-    }
-    return isValid;
-}
-
-function modalclose(){
-    var modalBg = document.querySelector('.modalbox');
-    modalBg.classList.remove('boxactive');
-}
-
-var closemodalbox = document.querySelector('.closemodal');
-
-closemodalbox.addEventListener('click',function(){
-    var modalBg = document.querySelector('.modalbox');
-    modalBg.classList.remove('boxactive');
-})
-
+//hide alert"
 var closedelalertbox = document.querySelector('.closedelalert');
 
 closedelalertbox.addEventListener('click',function(){
